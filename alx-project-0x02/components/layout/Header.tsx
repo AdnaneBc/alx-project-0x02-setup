65cb3ff07b5ab7f99fde0eaa2d59136a953998ff
Adnane BC <adnane.bachchar@gmail.com>
2025-06-02 10:49:04 +0100

    add navigation to header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
+    </>
+  );
 };
 
 export default Header;
