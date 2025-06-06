8964672dfba2476a2fb55fd08f1155619bac9be7
Adnane BC <adnane.bachchar@gmail.com>
2025-06-06 09:56:56 +0100

    add posts navigation

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,13 +1,35 @@
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 import React from "react";
 
-const posts = () => {
+// Define the props shape expected by the component
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <>
       <Header />
-      <div>posts</div>
+      <div>
+        {posts.map((post, index) => (
+          <PostCard key={index} post={post} />
+        ))}
+      </div>
     </>
   );
 };
 
-export default posts;
+export default Posts;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
